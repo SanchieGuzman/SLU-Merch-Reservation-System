9d3b8f9cdcbd64b5aaa6ac5580799ae5dfaef1a0
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2024-10-20 23:23:05 +0800

    implemented getTotalPendingOrders function in database.php

--- admin/src/database/database.php
+++ admin/src/database/database.php
@@ -84,6 +84,24 @@ class Database
     //todo: Create a function that fetches the number of pending orders of the organization a user is a part of
     public function getTotalPendingOrders($userID) {
         // kinukuha lng ung total pending orders
+        $stmt = $this->mysqli->prepare( "SELECT COUNT(DISTINCT(o.order_id)) AS total_pending 
+                                    FROM orders AS o 
+                                    JOIN order_products AS op USING (order_id)
+                                    JOIN products AS p USING (product_id)
+                                    WHERE p.organization_id IN (
+                                        SELECT om.organization_id 
+                                        FROM organization_members AS om 
+                                        JOIN vendors AS v USING (vendor_id)
+                                        JOIN users AS u 
+                                        USING(user_id) where u.user_id =1
+                                    )
+                                    AND o.status = 'pending'");
+        $stmt->bind_param('i', $userID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        return (int)$row['total_pending'];
     }
 
     //todo: Create a function that fetches the sales data of the organization a user is part of
