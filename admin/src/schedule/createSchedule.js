b3adcae879256693c9c49aab6571a09a96370b14
Leonhard Leung <Leonhardleung@gmail.com>
2024-11-11 20:54:52 +0800

    partial implementation of the create schedule page

---
+++ admin/src/schedule/createSchedule.js
@@ -0,0 +1,10 @@
+function close() {
+
+}
+
+function load() {
+    
+}
+
+const button = document.getElementById("create-schedule-button");
+button.onclick = load;
--- admin/src/sidenav/side-nav-route.php
+++ admin/src/sidenav/side-nav-route.php
@@ -1,18 +1,22 @@
 <?php
     session_start();
-    if($_POST['action'] === 'dashboard'){
+    if ($_POST['action'] === 'dashboard') {
         $_SESSION['action'] = 'dashboard';
         header("Location: ../dashboard/dashboard.php");
         exit();
-    }elseif($_POST['action'] === 'orders'){
+    } elseif($_POST['action'] === 'orders') {
         $_SESSION['action'] = 'orders';
         header("Location: ../orders/orders.php");
         exit();
-    }elseif ($_POST['action'] === 'products'){
+    } elseif ($_POST['action'] === 'products') {
         $_SESSION['action'] = 'products';
         header("Location: ../products/products.php");
         exit();
-    }elseif($_POST['action'] === 'logout'){
+    } elseif ($_POST['action'] === 'schedule') {
+        $_SESSION['action'] = 'schedule';
+        header("Location: ../schedule/schedule.php");
+        exit();
+    } elseif($_POST['action'] === 'logout') {
         $_SESSION['action'] = null;
         header("Location: ../login/login.php");
         exit();
