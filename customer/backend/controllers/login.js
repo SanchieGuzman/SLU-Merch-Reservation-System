1a6efce42d14918973a71b2fb67dc0b37edbc960
Stephen Coloma <2232847@slu.edu.ph>
2024-11-22 20:28:57 +0800

    add: added the template for the fetchAPI for the client side

--- customer/backend/controllers/login.js
+++ customer/backend/controllers/login.js
@@ -2,9 +2,15 @@ import Database from "../database/database.js";
 
 const loginController = async(req, res)=>{
     const db = Database.getInstance();
-    const result =  await db.login(req.body.username, req.body.password);    
 
-    res.json(result);
+    const result =  await db.login(req.body.username, req.body.password);
+    
+    if(result){        
+        res.sendStatus(200);
+    }else{       
+        res.status(400).json({message: 'Incorrect Credentials'});
+    }
+    
 }
 
 export default loginController;
