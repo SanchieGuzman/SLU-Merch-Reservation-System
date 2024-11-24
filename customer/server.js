925d4c4a7ad7df5b0bee1c2f1ec0e2e504f1d3a8
Stephen Coloma <2232847@slu.edu.ph>
2024-11-24 17:05:00 +0800

    fix: fixed the entire backend structure so that only one docker container will be used for entire customer app fixes include making frontend folder be statically served by the express server and a simple redirection on the index.html part

---
+++ customer/server.js
@@ -0,0 +1,32 @@
+// @ts-nocheck
+
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { serverConfig } from './backend/config.js';
+
+//router imports
+import loginRouter from './backend/routers/login.js';
+import productsRouter from './backend/routers/products.js';
+import _orgIDRouter from './backend/routers/_orgID.js';
+
+// Get __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//app
+const app = express();
+app.use(express.json())
+app.use(cookieParser());
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+//routers
+app.use('/api', loginRouter)
+// app.use('/api', productsRouter)
+app.use('/api', _orgIDRouter)
+
+app.listen(serverConfig.port, serverConfig.host, ()=>{
+    console.log(`Server app listening on host ${serverConfig.host} port ${serverConfig.port}`)
+})
