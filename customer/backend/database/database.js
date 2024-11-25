da0b3fa07efc0d9e1ec88be81c27be73f0695a55
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2024-11-25 20:41:07 +0800

    Added getProductDetails and firstFourProducts function in database.js

--- customer/backend/database/database.js
+++ customer/backend/database/database.js
@@ -32,7 +32,45 @@ class Database {
 
         return result[0];
     }
+    async getProductDetails(productID){
+        const query = 'SELECT product_id, product_name, product_image, product_description, price, quantity FROM products WHERE product_id =?';
+        const params = [productID];
+        const result = await this.execute(query, params);
+        return result[0];
+    }
+    async getFirstProductsOfEachOrg(){
+        const query = `
+                SELECT 
+                    p.product_id, 
+                    p.product_name,
+                    p.product_description,
+                    p.quantity,
+                    p.price, 
+                    p.organization_id,
+                    p.product_image,
+                    o.organization_name
+                FROM (
+                    SELECT 
+                        product_id, 
+                        product_name, 
+                        product_description, 
+                        quantity, 
+                        price, 
+                        organization_id, 
+                        product_image, 
+                        ROW_NUMBER() OVER (PARTITION BY organization_id ORDER BY product_id ASC) AS row_num 
+                    FROM products
+                ) AS p
+                JOIN organizations o USING(organization_id)
+                WHERE p.row_num <= 4; `;   
+        try {
+            const results = await this.execute(query);
+            return results; 
 
+        }catch(err){
+            console.log("Error executing queyr");
+        }
+    }
     // ETO ANG TEMPLATE FOR EXECUTING A QUERY. returns a promise object
     execute(query, params = []) {       
         return new Promise((resolve, reject) => {
